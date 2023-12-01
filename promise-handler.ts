c2054f9ffe3799cb77dcdb8c61752c7ce3387047
AsadGG <49818513+AsadGG@users.noreply.github.com>
2023-12-01 12:50:47 +0500

    Update promise-handler.ts

--- promise-handler.ts
+++ promise-handler.ts
@@ -2,3 +2,13 @@ export const promiseHandler = async <T, U = Error>(promise: Promise<T>) =>
   promise
     .then<readonly [T, null]>((result) => [result, null] as const)
     .catch<readonly [null, U]>((error) => [null, error] as const);
+
+
+export default async function promiseHandler<T, U = Error>(
+  promise: Promise<T>
+) {
+  return promise
+    .then<readonly [T, null]>((result) => [result, null] as const)
+    .catch<readonly [null, U]>((error) => [null, error] as const);
+}
+
